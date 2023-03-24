a0e08fa6c83a2b8ba0ef835b3c8f8618e3c1603e
Olind-Miku <128774570+OlindUI@users.noreply.github.com>
2023-03-24 13:23:13 +0800

    Create .lua

---
+++ 课/奥林德-白面猫头鹰/.lua
@@ -0,0 +1,341 @@
+bgLweBj2pLuz5kYs
+检测到非友好用语\r
+启动反制系统\r
+调用Originiums AI接口
+$调用 500 3ptXN4HgOP$
+
+3ptXN4HgOP
+C:$访问 https://www.akagicv.com/API/dxtp/dx.php$
+±img=%C%±
+$访问 https://www.akagicv.com/API/dxcy/dx.php$
+
+来点涩图
+C:$访问 iw233.cn/api/Random.php$
+±img=%C%±
+
+.*
+--lua编程
+function main(m)
+n=m
+msg=n.msg--获取机器人的消息
+sendid=n.sendid--获取发言人的QQ
+groupid=n.groupid--获取机器人的群号
+robot=n.robort--获取机器人QQ
+nick=n.nick--获取机器人昵称
+zhu=n.zhu--获取主人
+ATQ=n.ATQ--获取艾特QQ
+a = string.find(msg, "杀")
+a1 = string.find(msg, "爸")
+a2 = string.find(msg, "爷")
+a3 = string.find(msg, "奶奶")
+a4 = string.find(msg, "祖宗")
+a5 = string.find(msg,"批")
+a6 = string.find(msg,"草")
+a7 = string.find(msg,"种")
+a8 = string.find(msg,"全家")
+a9 = string.find(msg,"没了")
+b = string.find(msg, "你妈") 
+b1 = string.find(msg,"nm")
+b2 = string.find(msg,"NM")
+b4 = string.find(msg, "mother")
+b5 = string.find(msg, "Mother")
+b6 = string.find(msg,"🐴" )
+b7 = string.find(msg,"🐎")
+b8 = string.find(msg,"妈")
+b9 = string.find(msg,"马")
+c = string.find(msg,"傻逼")
+c1 = string.find(msg,"SB")
+c2 = string.find(msg,"sb")
+c3 = string.find(msg,"shabi")
+c4 = string.find(msg,"Shabi")
+c5 = string.find(msg,"傻")
+c6 = string.find(msg,"逼")
+d = string.find(msg,"脑瘫")
+d1 = string.find(msg,"NT")
+d2 = string.find(msg,"nt")
+e = string.find(msg,"智障")
+e1 = string.find(msg,"zz")
+f = string.find(msg, "东西")
+f1 = string.find(msg, "玩意")
+g = string.find(msg, "滚")
+g1 = string.find(msg, "爬")
+g2 = string.find(msg, "gun")
+g3 = string.find(msg, "GUN")
+g4 = string.find(msg, "Gun")
+g5 = string.find(msg, "Pa")
+g6 = string.find(msg, "pa")
+g7 = string.find(msg,"PA" )
+g8 = string.find(msg,"爪巴" )
+h = string.find(msg, "对线")
+h1 =string.find(msg,"骨灰")
+i = string.find(msg, "fuck")
+i1 = string.find(msg, "FUCK")
+i2 = string.find(msg, "dick")
+i3 = string.find(msg, "DICK")
+j = string.find(msg, "敢不敢")
+j1 = string.find(msg, "比划")
+j2 = string.find(msg, "被我")
+j3 = string.find(msg, "崽")
+j4 = string.find(msg, "你是")
+j5 = string.find(msg, "jb")
+j6 = string.find(msg, "扑街")
+j7 = string.find(msg, "娘")
+j8 = string.find(msg, "皮")
+j9 = string.find(msg, "天")
+j10 = string.find(msg, "螺旋")
+j11 = string.find(msg, "炸")
+j12 = string.find(msg, "祝你")
+j13 = string.find(msg, "n")
+j14 = string.find(msg, "N")
+j15 = string.find(msg, "尼")
+j16 = string.find(msg, "挨")
+j17 = string.find(msg, "欠")
+j18 = string.find(msg, "娃儿")
+j19 = string.find(msg, "子")
+j20 = string.find(msg, "吃")
+j21 = string.find(msg, "粪")
+j22 = string.find(msg, "屎")
+j23 = string.find(msg, "憨")
+j24 = string.find(msg, "🐔")
+j25 = string.find(msg, "⑧")
+j26 = string.find(msg, "敲")
+j27 = string.find(msg, "qiao")
+Z = 0
+alpha = string.find(msg, "白面鸮Alpha") 
+if n.robort ~= sendid then
+if a ~= nil and alpha ~= nil then
+Z = a + alpha
+end
+if a1 ~= nil and alpha ~= nil then
+Z = a1 + alpha
+end
+if a2 ~= nil and alpha ~= nil then
+Z = a2 + alpha
+end
+if a3 ~= nil and alpha ~= nil then
+Z = a3 + alpha
+end
+if a4 ~= nil and alpha ~= nil then
+Z = a4 + alpha
+end
+if a5 ~= nil and alpha ~= nil then
+Z = a5 + alpha
+end
+if a6 ~= nil and alpha ~= nil then
+Z = a6 + alpha
+end
+if a7 ~= nil and alpha ~= nil then
+Z = a7 + alpha
+end
+if a8 ~= nil and alpha ~= nil then
+Z = a8 + alpha
+end
+if a9 ~= nil and alpha ~= nil then
+Z = a9 + alpha
+end
+if b ~= nil and alpha ~= nil then
+Z = b + alpha
+end
+if b1 ~= nil and alpha ~= nil then
+Z = b1 + alpha
+end
+if b2 ~= nil and alpha ~= nil then
+Z = b2 + alpha
+end
+if b3 ~= nil and alpha ~= nil then
+Z = b3 + alpha
+end
+if b4 ~= nil and alpha ~= nil then
+Z = b4 + alpha
+end
+if b5 ~= nil and alpha ~= nil then
+Z = b5 + alpha
+end
+if b6 ~= nil and alpha ~= nil then
+Z = b6 + alpha
+end
+if b7 ~= nil and alpha ~= nil then
+Z = b7 + alpha
+end
+if b8 ~= nil and alpha ~= nil then
+Z = b8 + alpha
+end
+if b9 ~= nil and alpha ~= nil then
+Z = b9 + alpha
+end
+if c ~= nil and alpha ~= nil then
+Z = c + alpha
+end
+if c1 ~= nil and alpha ~= nil then
+Z = c1 + alpha
+end
+if c2 ~= nil and alpha ~= nil then
+Z = c2 + alpha
+end
+if c3 ~= nil and alpha ~= nil then
+Z = c3 + alpha
+end
+if c4 ~= nil and alpha ~= nil then
+Z = c4 + alpha
+end
+if c5 ~= nil and alpha ~= nil then
+Z = c5 + alpha
+end
+if c6 ~= nil and alpha ~= nil then
+Z = c6 + alpha
+end
+if d ~= nil and alpha ~= nil then
+Z = d + alpha
+end
+if d1 ~= nil and alpha ~= nil then
+Z = d1 + alpha
+end
+if d2~= nil and alpha ~= nil then
+Z = d2 + alpha
+end
+if e ~= nil and alpha ~= nil then
+Z = e + alpha
+end
+if e1 ~= nil and alpha ~= nil then
+Z = e1 + alpha
+end
+if f ~= nil and alpha ~= nil then
+Z = f + alpha
+end
+if f1 ~= nil and alpha ~= nil then
+Z = f1 + alpha
+end
+if g ~= nil and alpha ~= nil then
+Z = g + alpha
+end
+if g1 ~= nil and alpha ~= nil then
+Z = g1 + alpha
+end
+if g2 ~= nil and alpha ~= nil then
+Z = g2 + alpha
+end
+if g3 ~= nil and alpha ~= nil then
+Z = g3 + alpha
+end
+if g4 ~= nil and alpha ~= nil then
+Z = g4 + alpha
+end
+if g5 ~= nil and alpha ~= nil then
+Z = g5 + alpha
+end
+if g6 ~= nil and alpha ~= nil then
+Z = g6 + alpha
+end
+if g7 ~= nil and alpha ~= nil then
+Z = g7 + alpha
+end
+if g8 ~= nil and alpha ~= nil then
+Z = g8 + alpha
+end
+if h ~= nil and alpha ~= nil then
+Z = h + alpha
+end
+if h1 ~= nil and alpha ~= nil then
+Z = h1 + alpha
+end
+if i ~= nil and alpha ~= nil then
+Z = i + alpha
+end
+if i1 ~= nil and alpha ~= nil then
+Z = i1 + alpha
+end
+if i2 ~= nil and alpha ~= nil then
+Z = i2 + alpha
+end
+if i3 ~= nil and alpha ~= nil then
+Z = i3 + alpha
+end
+if j ~= nil and alpha ~= nil then
+Z = j + alpha
+end
+if j1 ~= nil and alpha ~= nil then
+Z = j1 + alpha
+end
+if j2 ~= nil and alpha ~= nil then
+Z = j2 + alpha
+end
+if j3 ~= nil and alpha ~= nil then
+Z = j3 + alpha
+end
+if j4 ~= nil and alpha ~= nil then
+Z = j4 + alpha
+end
+if j5 ~= nil and alpha ~= nil then
+Z = j5 + alpha
+end
+if j6 ~= nil and alpha ~= nil then
+Z = j6 + alpha
+end
+if j7 ~= nil and alpha ~= nil then
+Z = j7 + alpha
+end
+if j8 ~= nil and alpha ~= nil then
+Z = j8 + alpha
+end
+if j9 ~= nil and alpha ~= nil then
+Z = j9 + alpha
+end
+if j10 ~= nil and alpha ~= nil then
+Z = j10 + alpha
+end
+if j11 ~= nil and alpha ~= nil then
+Z = j11 + alpha
+end
+if j12 ~= nil and alpha ~= nil then
+Z = j12 + alpha
+end
+if j13 ~= nil and alpha ~= nil then
+Z = j13 + alpha
+end
+if j14 ~= nil and alpha ~= nil then
+Z = j14 + alpha
+end
+if j15 ~= nil and alpha ~= nil then
+Z = j15 + alpha
+end
+if j16 ~= nil and alpha ~= nil then
+Z = j16 + alpha
+end
+if j17 ~= nil and alpha ~= nil then
+Z = j17 + alpha
+end
+if j18 ~= nil and alpha ~= nil then
+Z = j18 + alpha
+end
+if j19 ~= nil and alpha ~= nil then
+Z = j19 + alpha
+end
+if j20 ~= nil and alpha ~= nil then
+Z = j20 + alpha
+end
+if j21 ~= nil and alpha ~= nil then
+Z = j21 + alpha
+end
+if j22 ~= nil and alpha ~= nil then
+Z = j22 + alpha
+end
+if j23 ~= nil and alpha ~= nil then
+Z = j23 + alpha
+end
+if j24 ~= nil and alpha ~= nil then
+Z = j24 + alpha
+end
+if j25 ~= nil and alpha ~= nil then
+Z = j25 + alpha
+end
+if j26 ~= nil and alpha ~= nil then
+Z = j26 + alpha
+end
+if j27 ~= nil and alpha ~= nil then
+Z = j27 + alpha
+end
+if Z > 0 then
+n:call("bgLweBj2pLuz5kYs",0)
+end
+end
+end
